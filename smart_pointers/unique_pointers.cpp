c8c2e73956954abd79c27e4174e0188e9ba99229
kamalasubha <kamalasubha@multicorewareinc.com>
2022-08-16 12:40:04 +0530

    Update Unique functions ivoke

--- smart_pointers/unique_pointers.cpp
+++ smart_pointers/unique_pointers.cpp
@@ -2,14 +2,92 @@
 #include <memory>
 #include <utility>
 
+// Class person with Name param
+class person {
+public:
+    std::string name;
+    person(std::string name) : name(name) {}
+    ~person() {
+        std::cout << "Person destructed." << std::endl;
+    }
+    void printHello() {
+        std::cout << "Hello World " << std::endl;
+    }
+    void printName() {
+        std::cout << "Name: " << name << std::endl;
+    }
+};
+
+// Pass to a function
+void passIn(std::unique_ptr<person> p) {
+    std::cout << "Inside passIn" << std::endl;
+    p->printName();
+}
+
+// Return unique pointer as a function return value
+std::unique_ptr<person> passOut() {
+    std::cout << "Inside passOut" << std::endl;
+    auto cand = std::make_unique<person>("Mike");
+    return cand;
+}
+
+// Oberver function
+void printName_pointer(person *p) {
+    p->printName();
+}
+
+// Oberver function
+void printName_reference(person &p) {
+    p.printName();
+}
+
 int main()
 {
-    std::unique_ptr<int> valuePtr;
-    valuePtr.reset(new int(42));
-    std::unique_ptr<int> valuePtrNow(std::move(valuePtr));
+    std::unique_ptr<person> cand_1;
+    std::unique_ptr<person> cand_2;
+    cand_1.reset(new person("John"));
+    cand_2 = std::make_unique<person>("Jane");
+
+    std::cout << "Candidate1 name = " << cand_1->name << '\n';
+    std::cout << "Candidate2 name = " << cand_2.get()->name << '\n';
+
+    std::swap(cand_1, cand_2);
  
-    std::cout << "valuePtrNow = " << *valuePtrNow << '\n';
-    std::cout << "Has valuePtr an associated object? "
-              << std::boolalpha
-              << static_cast<bool>(valuePtr) << '\n';
+    std::cout << "Candidate1 name = " << cand_1->name << '\n';
+    std::cout << "Candidate2 name = " << cand_2.get()->name << '\n';
+
+    std::unique_ptr<person> cand_3 = std::move(cand_1);
+
+    cand_1->printHello();
+    cand_3->printHello();
+
+    if (cand_1) {
+        cand_1->printName();
+    }
+    else {
+        std::cout << "cand_1 is null" << std::endl;
+    }
+    cand_3->printName();
+
+    passIn(std::move(cand_3));
+
+    if (cand_3) {
+        cand_3->printName();
+    }
+    else {
+        std::cout << "cand_3 is null" << std::endl;
+    }
+
+    cand_3 = passOut();
+    if (cand_3) {
+        cand_3->printName();
+    }
+    else {
+        std::cout << "cand_3 is null" << std::endl;
+    }
+
+    printName_pointer(cand_3.get());
+    printName_reference(*cand_3);
+
+    
 }
